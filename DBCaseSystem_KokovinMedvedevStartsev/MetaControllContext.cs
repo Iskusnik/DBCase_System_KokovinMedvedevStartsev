e0328fd6e7aab33225f205aa293aab9ee60691f9
Старцев Даниил Сергеевич <dsstartsev@edu.hse.ru>
2018-12-06 02:29:51 +0500

    CreateDB по метаданным

--- DBCaseSystem_KokovinMedvedevStartsev/MetaControllContext.cs
+++ DBCaseSystem_KokovinMedvedevStartsev/MetaControllContext.cs
@@ -29,22 +29,63 @@ namespace DBCaseSystem_KokovinMedvedevStartsev
 
             string[] tablesCreation = new string[tables.Length];
 
-
-
+            //Добавляет поля для создания ключа
             for (int i = 0; i < tables.Length; i++)
             {
                 //TODO: разобраться с SQL CREATE TABLE + понять, как создавать связи
-                tablesCreation[i] = "CREATE TABLE " + tables[i].Name;
+                //Поменять типы в схеме БД
+                //Надо ли делать новую БД?
+
+                string primaryKeys = "PRIMARY KEY (" + tables[i].Name + "Id";
+                string indexed = "";
+                tablesCreation[i] = "CREATE TABLE " + tables[i].Name + "(";
 
                 Attribute[] tablesAttributes = (from temp in attributes where temp.Table.Name == tables[i].Name select temp).ToArray();
 
+                //Генерируем айди - ключ
+                tablesCreation[i] += tables[i].Name + "Id int IDENTITY(1,1)";
+                
                 for (int j = 0; j < tablesAttributes.Length; j++)
-                    tablesCreation[i] += tablesAttributes[j].Name;
+                {
 
-                //В процессе
+                    tablesCreation[i] += tablesAttributes[j].Name + " ";
+                    tablesCreation[i] += tablesAttributes[j].Type + " ";
+
+                    if (tablesAttributes[j].Length != 0)
+                        tablesCreation[i] += "("+ tablesAttributes[j].Length.ToString() + ")" + " ";
+
+                    if (!tablesAttributes[j].IsNull)
+                        tablesCreation[i] +=  "NOT NULL" + " ";
+
+                    if (tablesAttributes[j].IsKey)
+                        primaryKeys += ", " + tablesAttributes[j].Name;
+                    
+                        tablesCreation[i] += ",";
+
+                    indexed = "CREATE INDEX " + "idx_" + tables[i].Name + "_" + tablesAttributes[j].Name +
+                    " ON " + tables[i].Name + "(" + tablesAttributes[j].Name + "); ";
+                }
+
+                tablesCreation[i] += primaryKeys + ")); ";
+                tablesCreation[i] += indexed;
+
 
             }
 
+            //Пример:
+            ///
+            ///     CREATE TABLE Persons (
+            ///     PersonsId  int IDENTITY(1,1),           - Автогенерируемый ключ - айди
+            ///     LastName varchar(255) NOT NULL,
+            ///     FirstName varchar(255),
+            ///     Age int,
+            ///     PRIMARY KEY(PersonsId)                  - Ключ задаётся
+            ///     );
+            /// 
+            ///     CREATE INDEX idx_Persons_LastName
+            ///     ON Persons(LastName);
+            /// 
+            ///
 
 
             command.ExecuteNonQuery();
